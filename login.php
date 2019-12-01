e373e5f9470773eaaf62e02745252a7815810719
tdt-hai <1760051@student.hcmus.edu.vn>
2019-12-01 22:23:34 +0700

    Update login.php

--- login.php
+++ login.php
@@ -132,14 +132,4 @@ if ($currentUser) {
         </div>
     </div>
 </div>
-<div class="alert alert-success alert-dismissible">
-                  <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
-                  <h5><i class="icon fas fa-check"></i> Alert!</h5>
-                  Success alert preview. This alert is dismissable.
-                </div>
-                <div class="alert alert-warning alert-dismissible">
-                  <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
-                  <h5><i class="icon fas fa-exclamation-triangle"></i> Alert!</h5>
-                  Warning alert preview. This alert is dismissable.
-                </div>
 <?php include 'footer.php'; ?>
